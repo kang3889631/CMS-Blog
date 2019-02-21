a4601cb5438cfc6a07d095492d3a88551fc0bc8a
shiyin weng <kang3889631@gmail.com>
2019-02-20 22:04:48 -0800

    2019-02-20

--- search.php
+++ search.php
@@ -10,9 +10,22 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
                 <?php
-                $query = "SELECT * FROM posts";
-                $select_all_posts_query = mysqli_query($connection,$query);
-                while($row=mysqli_fetch_assoc($select_all_posts_query)){
+                
+                    if(isset($_POST['search'])){
+                    $search = $_POST['search'];
+                    
+                    $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
+                    $search_query = mysqli_query($connection,$query);
+                    if(!$search_query){
+                        die("QUERY FAILED" . mysqli_error($connection));
+                    }
+                    $count = mysqli_num_rows($search_query);
+                    if($count==0){
+                        echo "<h1> NO RESULT</h1>";
+                    }else{
+//                         $query = "SELECT * FROM posts";
+//                $select_all_posts_query = mysqli_query($connection,$query);
+                while($row=mysqli_fetch_assoc($search_query)){
                         $post_title = $row['post_title'];
                         $post_author = $row['post_author'];
                         $post_date = $row['post_date'];
@@ -41,7 +54,11 @@
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
-                <?php }  ?>
+                <?php }  
+                    }
+                } ?>
+                                
+               
 
             </div>
 
