8e9b27cf5aef3ca0f7ab1c11f2796d59a72b4f59
shiyin weng <kang3889631@gmail.com>
2019-02-22 20:30:05 -0800

    Update

--- includes/navigation.php
+++ includes/navigation.php
@@ -22,6 +22,8 @@
                         echo "<li><a href='#'>{$cat_title}</a></li>";
                     }
                     ?>
+                    <li><a href="admin">Admin</a></li>
+                    
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
