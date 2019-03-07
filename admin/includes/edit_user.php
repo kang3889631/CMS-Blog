3ab13c80ee9e485548a1706c8f384fa1e0f7388b
shiyin weng <kang3889631@gmail.com>
2019-03-07 05:18:25 -0800

    users

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -91,6 +91,29 @@ $cat_title = $row['cat_title'];
     </div>
     
     <div class="form-group">
+    <select name="user_role" id="">
+    
+<?php
+    $query = "SELECT * FROM users";
+    $select_users = mysqli_query($connection,$query);
+
+    confirmQuery($select_users);  
+
+    while($row=mysqli_fetch_assoc($select_users)){
+    $user_id = $row['user_id'];
+    $user_role = $row['user_role'];
+
+    echo "<option value={$user_id}>{$user_role}</option>";
+    }
+?>
+        
+        
+    </select>
+    </div>
+    
+    
+    
+    <div class="form-group">
         <label for="post_status">Post Status</label>
         <input value="<?php echo $post_status; ?>" type="text" class="form-control" name="post_status">
     </div>
