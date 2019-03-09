b2c59aa9f4cba4426528cc982820f3ec87df7246
shiyin weng <kang3889631@gmail.com>
2019-03-09 15:13:05 -0800

    login&logout, Profile

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -2,6 +2,23 @@
 <?php include "../includes/db.php"; ?>
 <?php include "functions.php"; ?>
 <?php session_start(); ?>
+
+<?php 
+
+if(!isset($_SESSION['user_role'])){
+
+header("Location: ../index.php");
+
+}else{
+    if($_SESSION['user_role']!=='admin'){
+        header("Location: ../index.php");
+    }
+}
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
--- admin/profile.php
+++ admin/profile.php
@@ -1,24 +1,31 @@
-<?php
-if(isset($_GET['edit_user'])){
-        $the_user_id = $_GET['edit_user'];
-    }
-    $query = "SELECT * FROM users WHERE user_id={$the_user_id}";
-    $select_user_by_id = mysqli_query($connection,$query);
+<?php include "includes/admin_header.php"; ?>
 
-while($row=mysqli_fetch_assoc($select_user_by_id)){
-    //$post_id = $row['post_id'];
+<?php
+
+if(isset($_SESSION['username'])){
+    
+    $username = $_SESSION['username'];
+    
+    $query = "SELECT * FROM users WHERE username = '{$username}' ";
+    
+    $select_user_profile_query = mysqli_query($connection, $query);
+    
+    while($row = mysqli_fetch_assoc($select_user_profile_query)){
+        $user_id = $row['user_id'];
+        $username = $row['username'];
+        $user_password = $row['user_password'];
         $user_firstname = $row['user_firstname'];
         $user_lastname = $row['user_lastname'];
-        $user_role = $row['user_role'];
-        $username = $row['username'];
         $user_email = $row['user_email'];
-        $user_password = $row['user_password'];
-    
-//        $post_content = $row['post_content'];
-//        $post_comment_count = $row['post_comment_count'];
-//        $post_date = $row['post_date'];
+        $user_image = $row['user_image'];
+        $user_role = $row['user_role'];
     }
+}
 
+
+?>
+   
+<?php
 if(isset($_POST['edit_user'])){
     
 //    $user_id = $_POST['user_id'];
@@ -46,18 +53,36 @@ if(isset($_POST['edit_user'])){
     $query .= "user_password = '{$user_password}' ";
 //    $query .= "post_content = '{$post_content}', ";
 //    $query .= "post_image = '{$post_image}' ";
-    $query .= "WHERE user_id = {$the_user_id}";
+    $query .= "WHERE username = '{$username}' ";
 
     $update_user = mysqli_query($connection, $query);
 
     confirmQuery($update_user);
 }
-    
+
 
 ?>
-    
+   
+   
+   
+    <div id="wrapper">
 
-<form action="" method="post" enctype="multipart/form-data">
+        <!-- Navigation -->
+        <?php include "includes/admin_navigation.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                       
+                       <h1 class="page-header">
+                            Welcome to admin
+                            <small><?php echo $_SESSION['username']; ?></small>
+                        </h1>
+    <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="post_author">Firstname</label>
         <input value="<?php echo $user_firstname; ?>" type="text" class="form-control" name="user_firstname">
@@ -70,12 +95,12 @@ if(isset($_POST['edit_user'])){
     
     <div class="form-group">
     <select name="user_role" id="">
-        <option value="ubscriber"><?php echo $user_role; ?></option>
+        <option value="subscriber"><?php echo $user_role; ?></option>
         <?php
         if($user_role=='admin'){
-            echo "<option value='subscriber'>Subscriber</option>";
+            echo "<option value='subscriber'>subscriber</option>";
         }else{
-            echo "<option value='admin'>Admin</option>";
+            echo "<option value='admin'>admin</option>";
         }
         ?>
         
@@ -107,7 +132,18 @@ if(isset($_POST['edit_user'])){
     </div>
     
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="edit_user" value="Update">
+        <input class="btn btn-primary" type="submit" name="edit_user" value="Update Profile">
     </div>
 
 </form>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    <?php include "includes/admin_footer.php"; ?>
