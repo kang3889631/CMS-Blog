3f4f3b06d3e721e376454372fd4c58db256aa519
shiyin weng <kang3889631@gmail.com>
2019-03-10 21:27:53 -0700

    update

--- admin/index.php
+++ admin/index.php
@@ -158,6 +158,11 @@
                 <!-- /.row -->
                 <?php
                 
+                $query  = "SELECT * FROM posts WHERE post_status = 'published'";
+                $select_all_published_post = mysqli_query($connection,$query);  
+                $post_published_count = mysqli_num_rows($select_all_published_post);
+                
+                
                 $query  = "SELECT * FROM posts WHERE post_status = 'draft'";
                 $select_all_draft_post = mysqli_query($connection,$query);  
                 $post_draft_count = mysqli_num_rows($select_all_draft_post);
@@ -185,10 +190,10 @@
             
             <?php
             
-            $element_text = ['Active Posts','Draft Posts', 'Comments','Pending Comments', 'Users','Subscribers', 'Categories'];
-            $element_count = [$post_count, $post_draft_count, $comment_count, $comment_unapproved_count, $user_count,$subscriber_count, $category_count];
-            
-            for($i=0;$i<7;$i++){
+            $element_text = ['All Posts','Active Posts','Draft Posts', 'Comments','Pending Comments', 'Users','Subscribers', 'Categories'];
+            $element_count = [$post_count,$post_published_count, $post_draft_count, $comment_count, $comment_unapproved_count, $user_count,$subscriber_count, $category_count];
+        
+            for($i=0;$i<8;$i++){
                 echo "['{$element_text[$i]}'" . "," . "{$element_count[$i]}],"; 
             }
             
